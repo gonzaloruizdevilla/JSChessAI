1f8eca9d52f228c6a7224b44143b926b006fbec2
Gonzalo Ruiz de Villa <gonzaloruizdevilla@gmail.com>
2018-10-03 12:25:42 +0200

    config and onDragStart

--- main.js
+++ main.js
@@ -1,6 +1,25 @@
 let board,
     game = new Chess();
 
+let onDragStart = 
+    (square, piece) =>
+        !(game.in_checkmate() || game.in_draw() || /^b/.test(piece))
+
+let onMouseoverSquare = (...args) => { }
+let onMouseoutSquare = (...args) => {  }
+let onDrop = (...args) => {  }
+let onSnapEnd = (...args) => {  }
 
 
-board = ChessBoard('board', 'start');
+
+let config = {
+    draggable: true,
+    position: 'start',
+    onDragStart,
+    onDrop,
+    onMouseoutSquare,
+    onMouseoverSquare,
+    onSnapEnd
+}
+
+board = ChessBoard('board', config);
