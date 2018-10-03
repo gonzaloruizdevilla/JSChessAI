34f9c9f67f1b87b3ba9274e201ece881add36a6b
Gonzalo Ruiz de Villa <gonzaloruizdevilla@gmail.com>
2018-10-03 12:48:31 +0200

    onMouseoverSquare and onMouseoutSquare

--- main.js
+++ main.js
@@ -5,8 +5,20 @@ let onDragStart =
     (square, piece) =>
         !(game.in_checkmate() || game.in_draw() || /^b/.test(piece))
 
-let onMouseoverSquare = (...args) => { }
-let onMouseoutSquare = (...args) => {  }
+let highlightSquare = 
+    square => document.querySelector(`.square-${square}`).classList.toggle('highlight')
+
+let removeHighlight = 
+    () => document.querySelectorAll('.highlight').forEach(el => el.classList.toggle('highlight'))
+
+let onMouseoverSquare =
+    (square, piece) => {
+        var moves = game.moves({ square, verbose: true }).map(({to}) => to);
+        [...moves, square].forEach(highlightSquare)
+    }
+
+let onMouseoutSquare = removeHighlight
+
 let onDrop = (...args) => {  }
 let onSnapEnd = (...args) => {  }
 
