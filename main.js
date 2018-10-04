070a6dfc68dfbf969db06777250b2da140a9c83d
Gonzalo Ruiz de Villa <gonzaloruizdevilla@gmail.com>
2018-10-04 10:30:01 +0200

    onDrop and onSnapEnd

--- main.js
+++ main.js
@@ -17,11 +17,42 @@ let onMouseoverSquare =
         [...moves, square].forEach(highlightSquare)
     }
 
-let onMouseoutSquare = removeHighlight
+let onMouseoutSquare = removeHighlight;
 
-let onDrop = (...args) => {  }
-let onSnapEnd = (...args) => {  }
+let renderHistory = () => document.getElementById("history").innerHTML = `
+    ${game.history().map(move => `
+        <span>${move}</span>
+    `).join('')}
+`;
 
+let findBestMove = () => {
+    if (game.game_over()) return alert('Game over!');
+    let moves = game.moves();
+    return moves[0|Math.random()*moves.length]
+}
+
+let makeBestMove = () => {
+    let bestMove = findBestMove();
+    if (bestMove) {
+        game.move(bestMove);
+        board.position(game.fen())
+        renderHistory();
+        if (game.game_over()) alert('Game over!');
+    }
+
+}
+
+let onDrop = (from, to) => {
+    removeHighlight();
+    var move = game.move({from, to, promotion:'q'});
+    if (!move) return 'snapback';
+    renderHistory();
+    window.setTimeout(makeBestMove, 500);
+
+}
+let onSnapEnd = () => {
+    board.position(game.fen());
+}
 
 
 let config = {
